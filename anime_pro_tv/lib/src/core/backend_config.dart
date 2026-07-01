589b09ea6a3864e35d286d2c14a776b3764bd1ad
YourAnsh <88235820+Ansh7473@users.noreply.github.com>
2026-07-01 10:43:30 +0530

    Update backend_config.dart

--- anime_pro_tv/lib/src/core/backend_config.dart
+++ anime_pro_tv/lib/src/core/backend_config.dart
@@ -8,8 +8,8 @@ class Backend {
 
   /// Backend hosts (two separate Vercel accounts).
   static const List<String> hosts = [
-    'https://anime-pro-backend.vercel.app',
-    'https://anime-pro-backend-smoky.vercel.app',
+    'https://animeback-d76691b0d2f2.herokuapp.com',
+    'https://animeback-d76691b0d2f2.herokuapp.com',
   ];
 
   static int _cursor = 0;
